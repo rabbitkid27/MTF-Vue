334eb48f5f6d022f9923ad19c31d3620278633a9
rabbitkid27 <544955525@qq.com>
2019-01-13 11:57:56 +0800

    還沒吃飯

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,50 @@
 <template>
-  <div id="app">
-
+  <div class="login-container">
+    <div class="form-container">
+      <h2>用戶登錄頁面</h2>
+      <el-form :label-position="top" label-width="80px" :model="formData">
+        <el-form-item label="用戶名">
+          <el-input v-model="formData.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密碼">
+          <el-input v-model="formData.password"></el-input>
+        </el-form-item>
+          <el-button class="login-btn" type="success">登錄</el-button>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'app',
-  components: {
-    
-  }
+  data() {
+    return {
+      formData:{
+        username:"",
+        password:"",
+      }
+    }
+  },
 }
 </script>
 
-<style>
+<style lang="scss">
+.login-container{
+  background-color: #324152;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .form-container {
+    background-color: white;
+    width: 580px;
+    padding: 40px;
+    box-sizing: border-box;
+    border-radius: 10px;
+  }
+  .login-btn{
+    width: 100%;
+  }
+}
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 引入全局樣式
+import './assets/base.css'
+
+// 導入EL-UI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
 Vue.config.productionTip = false
 
 new Vue({
